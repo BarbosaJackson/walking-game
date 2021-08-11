6ff7db7fb30099e50757eda51b5e997c789808eb
Jackson Barbosa <jbs@ic.ufal.br>
2021-08-10 23:40:10 -0300

    tirando coisas não usadas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-let movementDelay = 100, pauseDelay = 1000, maxDelay = 2000, minDelay = 100;
 let game = null;
-let stop, walk;
 function preload() {
 	this.game = new Game();
 }
@@ -17,7 +15,7 @@ async function draw() {
 
 function keyReleased() {
    const keyName = event.key;
-   let dx = 0, dy = 0, arrow = '0';
+   let dx = 0, dy = 0;
    if(keyName == 'ArrowUp') {
    	if(this.game.x) {
    		dx = -1;
@@ -39,28 +37,3 @@ function keyReleased() {
    	this.game.update(dx, dy);
    }
 }
-
-// document.addEventListener('keydown', (event) => {
-//   const keyName = event.key;
-//   let dx = 0, dy = 0;
-//   if(keyName == 'ArrowUp') {
-//   	if(this.game.x) {
-// 		dx = -1;
-//   	}
-//   } else if(keyName == 'ArrowDown') {
-//   	if(this.game.x + 1 < this.game.n) {
-//   		dx = 1;
-//   	}
-//   } else if(keyName == 'ArrowRight'){
-//   	if(this.game.y + 1 < this.game.m) {
-//   		dy = 1;
-//   	}
-//   } else if(keyName == 'ArrowLeft') {
-//   	if(this.game.y) {
-//   		dy = -1;
-//   	}
-//   }
-//   if(dx || dy) {
-// 		this.game.update(dx, dy);
-//   }
-// });
